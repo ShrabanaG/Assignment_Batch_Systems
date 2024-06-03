dbf9a451a46d48e7a5945c465b02c589431d7449
ShrabanaG <shrabanagoswami8@gmail.com>
2024-06-03 23:58:19 +0530

    feat: .

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export { default as Hero } from "./hero/Hero.tsx";
-export {default as Features} from "./features/Features.tsx";
-export { default as Navbar} from "./navbar/Navbar.tsx";
-export { default as Advantages} from "./advantages/Advantages.tsx";
-export { default as Testimonials} from "./testimonials/Testimonials.tsx";
-export { default as Banner } from "./banner/Banner.tsx";
-export { default as FAQ} from "./faq/FAQ.tsx";
-export { default as Footer} from "./footer/Footer.tsx";
+export { default as Hero } from "./hero/Hero";
+export {default as Features} from "./features/Features";
+export { default as Navbar} from "./navbar/Navbar";
+export { default as Advantages} from "./advantages/Advantages";
+export { default as Testimonials} from "./testimonials/Testimonials";
+export { default as Banner } from "./banner/Banner";
+export { default as FAQ} from "./faq/FAQ";
+export { default as Footer} from "./footer/Footer";
